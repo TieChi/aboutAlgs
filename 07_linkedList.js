122b0aed6555ae252d03d7f15edfe9525d1c0737
ZT158\hanjun <hanjun@chint.com>
2018-10-09 18:10:47 +0800

    增加两种单链表翻转方法

--- 07_linkedList.js
+++ 07_linkedList.js
@@ -44,14 +44,70 @@ class LinkedList {
         
         return this.head;
     }
-    // 翻转单链表
+    // 翻转单链表 方法名不明
     reverseList() {
-        let p = this.head;
-        while (p.next != null) {
-            p.next.next = p;
+        let source, head;
+        // 首先截去头部空节点简化翻转过程
+        head = this.head.next;
+        this.head = new ListNode(null);
+        // 如果去掉空节点的链表长度大于1
+        if (head && head.next) {
+            let p = head;
+            let q = head.next;
+            // 切断p到q的指向
+            p.next = null;
+            // 以q为起点开始循环
+            while (q != null) {
+                // 源链表以q的next为起点
+                // 将q的next指向p，把q从旧链表头部去掉并加入新链表头部
+                source = q.next;
+                q.next = p;
+                // 新链表的头部（p）是当前的q
+                // 旧链表头部（q）是当前source
+                p = q;
+                q = source;
+            }
+            this.head.next = p;
+        } else {
+            this.head.next = head;
         }
-        
+
         return this.head;
+    }
+    // 尾插法 反转单链表
+    reverse2List() {
+        // 定义新链表
+        const root = new ListNode(null);
+        // 跳过作为占位的头部空节点
+        let currentNode = this.head.next;
+        // 只要当前节点没不为空就可以一直向下继续寻找
+        while (currentNode != null) {
+            // 可能为null的next节点
+            const next = currentNode.next;
+            // 在当前节点后接入新链表除头部空节点主体
+            // 把当前节点接入新链表头部空节点后面
+            currentNode.next = root.next;
+            root.next = currentNode;
+            // 移动到当前节点的next节点
+            currentNode = next;
+        }
+        this.head = root;
+    }
+    reverse3List() {
+        let t = null;
+        let p = this.head;
+        let q = this.head.next;
+        let next = null;
+        while(q != null){
+            console.log(t);
+            next = q.next;
+            q.next = p;
+            p.next = t;
+            t = p;
+            p = q;
+            q = next;
+        }
+        this.head = p;
     }
     // 向后接入其它链表
     concatList(addition) {
@@ -68,14 +124,26 @@ class LinkedList {
     }
 }
 
-let myLink1 = new LinkedList();
-myLink1.chainList([1, 3, 5]);
-let myLink2Head = new LinkedList().chainList([2, 4, 6]);
-myLink1.concatList(myLink2Head);
-console.log(myLink1.getAll());
+// let myLink1 = new LinkedList();
+// myLink1.chainList([1, 3, 5, 7, 9]);
+// let myLink2Head = new LinkedList().chainList([2, 4, 6, 8]);
+// myLink1.concatList(myLink2Head);
+// console.log(myLink1.getAll());
 
-let myList3 = new LinkedList();
-myList3.chainList([1, 2, 3]).reverseList();
-console.log(myList3);
+// let myList3 = new LinkedList();
+// myList3.chainList([1, 3, 5, 7, 9]);
+// console.log(myList3.getAll());
+// myList3.reverseList();
+// console.log(myList3.getAll());
 
+// let myList4 = new LinkedList();
+// myList4.chainList([0, 2, 4, 6, 8]);
+// console.log(myList4.getAll());
+// myList4.reverse2List();
+// console.log(myList4.getAll());
 
+let myList5 = new LinkedList();
+myList5.chainList([0, 2, 4, 6, 8]);
+console.log(myList5.getAll());
+myList5.reverse3List();
+console.log(myList5.getAll());
