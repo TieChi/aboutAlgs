9823bd387238e3dc13630c4ecf3b712d552e7e9c
ZT158\hanjun <hanjun@chint.com>
2018-10-18 20:00:40 +0800

    添加数组方法实现的循环队列

--- 09_queue.js
+++ 09_queue.js
@@ -1,7 +1,7 @@
 
 /*
 * 基于数组的队列
-* */
+*/
 class Queue {
     constructor(len) {
         this.arr = [1, 3, 5, 7, 9];
@@ -15,7 +15,7 @@ class Queue {
         // 从head出发寻找tail的位置
         let count = this.head;
         while (this.arr[count]) {
-            this.tail = count;
+            this.tail = count + 1;
             count++;
         }
 
@@ -25,13 +25,15 @@ class Queue {
         console.log(this.arr);
     }
     enqueue(n) {
-        this.init();
         if (this.head !== 0 && this.tail === this.arr.length) {
             for (let i = this.head; i < this.tail; i++) {
                 this.arr[i - this.head] = this.arr[i];
+                this.arr[i] = void 0;
             }
+            // 在重置队列中元素后重新获取head和tail的位置
+            this.tail -= this.head;
+            this.head = 0;
         }
-        console.log(this.arr);
         if (this.tail !== this.arr.length) {
             this.arr[this.tail] = n;
             ++this.tail;
@@ -47,7 +49,43 @@ class Queue {
     }
 }
 
-new Queue(5).init()
-            .dequeue().dequeue()
-            .enqueue(21)
-            .displayAll();
+// 基于数组的循环队列
+class CircularQueue {
+    constructor(len) {
+        this.arr = [1, 3, 5];
+        this.arr.length = len;
+        this.head = 0;
+        this.tail = 0;
+    }
+    // 如果初始arr不为空需要首先获取tail的位置
+    init() {
+        // 从head出发寻找tail的位置
+        let count = this.head;
+        while (this.arr[count]) {
+            this.tail = count + 1;
+            count++;
+        }
+
+        return this;
+    }
+    displayAll() {
+        console.log(this.arr);
+    }
+    enqueue(n) {
+        if (this.tail % this.arr.length !== this.head) {
+            this.arr[this.tail] = n;
+            this.tail = (this.tail + 1) % this.arr.length;
+
+        }
+
+        return this;
+    }
+    dequeue() {
+        this.arr[this.head] = void 0;
+        this.head = (this.head + 1) % this.arr.length;
+
+        return this;
+    }
+}
+
+new CircularQueue(5).init().dequeue().enqueue(13).enqueue(15).enqueue(23).displayAll();
