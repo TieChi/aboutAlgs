b8f69c16bd7b11197a95c0bf06cf596b0f34e3c8
ZT158\hanjun <hanjun@chint.com>
2018-10-22 17:55:39 +0800

    添加原地快速排序

--- 12_sort2.js
+++ 12_sort2.js
@@ -1,6 +1,7 @@
 /*
 * 冒泡排序和插入排序无论如何都是要多次遍历所有的数据，时间复杂度为O(n^2)。
 * 大规模的数据排序需要用到分治思想或者说用到递归。
+* **归并排序能不能改造成原地排序？为什么？
 * */
 let arr = [1, 6, 11, 5, 16, 0, 9, 4, 15, 18, 8, 2, 7, 19, 17, 14, 13, 10, 3, 12];
 
@@ -47,7 +48,7 @@ class mergeSort {
 * 快速排序
 * */
 
-class QuickSort {
+class QuickSort1 {
     constructor() {
 
     }
@@ -70,5 +71,52 @@ class QuickSort {
     };
 }
 
-console.log(new QuickSort().sort(arr));
+class QuickSort2 {
+    constructor(arr) {
+        this.arr = arr;
+    }
+    // 快速排序递归函数，p和r为下标
+    quickSortInternally(p, r) {
+        // 当数组长度为0或1时终止调用
+        if (p >= r) {
+            return;
+        }
+        // 该函数有两个作用：
+        // return获取分割点
+        // sideeffect为arr中p到r中的值被q平分（不一定按顺序）
+        let q = this.partition(p, r);
+        // 按分割点q分别递归地对数组的两部分（大于或小于arr[q]）,
+        // 调用排序（按q分割）
+        // 注意：因为arr[q]在arr[p]到arr[r]中排位不固定，
+        //       所以q在p到r中的位置也不固定，
+        //       所以arr中p到q-1与q+1到r的长度和长度比例也不固定
+        this.quickSortInternally(p, q-1);
+        this.quickSortInternally(q+1, r);
+
+        return this.arr;
+    }
+    partition(p, r) {
+        // 取数组最后一个值为分割点
+        let pivot = this.arr[r];
+        // 定义i和j两个都从p开始，但自增规则不同的变量
+        let i = p;
+        let j = p;
+        // 循环p到r所有值，
+        // 把所有小于pivot的值交换到i前面
+        for(; j < r; ++j) {
+            if (this.arr[j] < pivot) {
+                [this.arr[i], this.arr[j]] = [this.arr[j], this.arr[i]];
+                ++i;
+            }
+        }
+        // 把分割点pivot（a[r]）交换到a[i]位置
+        [this.arr[i], this.arr[r]] = [this.arr[r], this.arr[i]];
+
+        return i;
+    }
+}
+
+// 是原地排序所以也可以不取返回值，只利用函数的sideeffect
+let len = arr.length;
+console.log( new QuickSort2(arr).quickSortInternally(0, (len-1)) );
 
