e9da50c4b68757c3702d538fbf558dc5875ea011
ZT158\hanjun <hanjun@chint.com>
2019-02-22 17:49:50 +0800

    二叉树删除方法

--- 23_binary_tree1.js
+++ 23_binary_tree1.js
@@ -2,7 +2,7 @@
 * 二叉树的遍历
 * */
 
-let arr = [ 10, 24, 28, 43, 44, 53, 79, 81, 86, 95 ];
+let arr = [1, 6, 11, 5, 16, 0, 9, 4, 15, 18, 8, 2, 7, 19, 17, 14, 13, 10, 3, 12];
 
 class TreeNode{
     constructor(data, left, right) {
@@ -16,35 +16,134 @@ class BinaryTree{
     constructor() {
         this.root = new TreeNode(null, null, null);
     }
-    insert(data) {
+    // 简洁标准的插入方法
+    insert1(data) {
+        if (!this.root.data) {
+            this.root = new TreeNode(data, null, null);
+        } else {
+            var current = this.root;
+            var parent;
+            while (true) {
+                parent = current;
+                if (data < current.data) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = new TreeNode(data, null, null);
+                        break;
+                    }
+                }
+                else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = new TreeNode(data, null, null);
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+    // 啰嗦但更容易理解的插入方法
+    insert2(data) {
         if (!this.root.data) {
             this.root = new TreeNode(data, null, null);
         } else {
             let parent;
             let current = this.root;
-            while (current.data) {
+            while (current && current.data) {
                 parent = current;
                 if (data < parent.data) {
-                    current = parent.left;
+                    current = current.left;
                 }
                 else {
-                    current = parent.right;
+                    current = current.right;
                 }
             }
-            current.data = new TreeNode(data, null, null);
+            if (data < parent.data) {
+                parent.left = new TreeNode(data, null, null);
+            }
+            else {
+                parent.right = new TreeNode(data, null, null);
+            }
         }
         return this;
     }
     insertArr(arr) {
         let i = 0;
         while (i < arr.length) {
-            this.insert(arr[i]);
+            this.insert2(arr[i]);
             ++i;
         }
+        return this;
     }
-    // Preorder() {
-    //
-    // }
+    find(data) {
+        var current = this.root;
+        while (current && current.data) {
+            if (data === current.data) {
+                return current;
+            } else if (data < current.data) {
+                current = current.left;
+            } else if (data > current.data) {
+                current = current.right;
+            }
+        }
+        return null;
+    }
+    remove(data) {
+        this.removeNode(this.root, data);
+        return this;
+    }
+    removeNode(node, data) {
+        if (node == null) {
+            return null;
+        }
+        if (data == node.data) {
+            let current = node;
+            // 没有子节点的节点
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            // 没有左子节点的节点
+            if (node.left == null) {
+                return node.right;
+            }
+            // 没有右子节点的节点
+            if (node.right == null) {
+                return node.left;
+            }
+            // 有两个子节点的节点
+            var tempNode = this.getSmallest(node.right);
+            node.data = tempNode.data;
+            node.right = removeNode(node.right, tempNode.data);
+            return node;
+        }
+        else if (data < node.data) {
+            node.left = this.removeNode(node.left, data);
+            return node;
+        }
+        else {
+            node.right = this.removeNode(node.right, data);
+            return node;
+        }
+    }
+    getSmallest(node) {
+        let current = node;
+        if (node.left) {
+            current = node.left;
+        }
+        return current;
+    }
+    // 中序遍历
+    midOrderTree() {
+        this.midOrder(this.root);
+    }
+    midOrder(node) {
+        if (node && node.data) {
+            this.midOrder(node.left);
+            console.log(node.data);
+            this.midOrder(node.right);
+        }
+    }
 }
 
-new BinaryTree().insertArr(arr);
+console.log(new BinaryTree().insertArr(arr).remove(12).midOrderTree());
