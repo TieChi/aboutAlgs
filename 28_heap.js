32e4b535aa557b935173167fd55cc57c43f9b2e8
ZT158\hanjun <hanjun@chint.com>
2019-02-27 16:56:54 +0800

    堆取最大值方法

--- 28_heap.js
+++ 28_heap.js
@@ -34,9 +34,37 @@ class Heap{
         });
         return this;
     }
+    removeTop() {
+        if (this.count === 0) {
+            return null;
+        } else {
+            let top = this.arr[0];
+            this.arr[0] = this.arr[this.count - 1];
+            --this.count;
+            this.heapify();
+            return top;
+        }
+    }
+    heapify() {
+        let i = 0;
+        while (true) {
+            let maxPos = i;
+            if (i*2+1 <= this.count && this.arr[maxPos] < this.arr[i*2]) maxPos = i*2+1;
+            if (i*2+2 <= this.count && this.arr[maxPos] < this.arr[i*2+1]) maxPos = i*2+2;
+            if (maxPos === i) break;
+            [this.arr[i], this.arr[maxPos]] = [this.arr[maxPos], this.arr[i]];
+            i = maxPos;
+        }
+    }
+
     display() {
         return this.arr;
     }
 }
-let arr = [12, 2, 22, 42, 32];
-console.log(new Heap().init(15).insertArr(arr).display());
+
+let arr = [12, 2, 22, 42, 32, 21, 24, 25];
+let ex = new Heap().init(15).insertArr(arr);
+console.log(ex.display());
+console.log(ex.removeTop());
+console.log(ex.display());
+
