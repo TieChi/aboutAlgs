6b7ae4b482f5fcda519d7593f0c2d9643ac17e9d
ZT158\hanjun <hanjun@chint.com>
2019-02-28 13:49:55 +0800

    修正堆结构中模糊的取出顶部节点方法，并添加注释

--- 28_heap.js
+++ 28_heap.js
@@ -8,7 +8,7 @@ class HeapNode{
 class Heap{
     constructor(arr){
         this.arr = [];
-        this.count = 0;
+        this.count = 1;
     }
     init(capacity) {
         this.arr.length = capacity;
@@ -20,7 +20,7 @@ class Heap{
         }
         this.arr[this.count] = data;
         let i = this.count;
-        while (parseInt(i/2) >= 0 && this.arr[i] > this.arr[parseInt(i/2)]) {
+        while (parseInt(i/2) > 0 && this.arr[i] > this.arr[parseInt(i/2)]) {
             [this.arr[i], this.arr[parseInt(i/2)]] = [this.arr[parseInt(i/2)], this.arr[i]];
             i = parseInt(i/2);
         }
@@ -35,36 +35,46 @@ class Heap{
         return this;
     }
     removeTop() {
-        if (this.count === 0) {
+        if (this.count === 1) {
             return null;
         } else {
-            let top = this.arr[0];
-            this.arr[0] = this.arr[this.count - 1];
+            let top = this.arr[1];
+            this.arr[1] = this.arr[this.count - 1];
+            this.arr[this.count - 1] = void 0;
             --this.count;
             this.heapify();
             return top;
         }
     }
     heapify() {
-        let i = 0;
+        let i = 1;
         while (true) {
+            // 获取当前操作节点和它的两个子节点中最大的那个
             let maxPos = i;
-            if (i*2+1 <= this.count && this.arr[maxPos] < this.arr[i*2]) maxPos = i*2+1;
-            if (i*2+2 <= this.count && this.arr[maxPos] < this.arr[i*2+1]) maxPos = i*2+2;
-            if (maxPos === i) break;
+            // 如果操作节点的值小于子节点，就将maxPos下沉到该子节点
+            if (i*2 <= this.count && this.arr[maxPos] < this.arr[i*2]) {
+                maxPos = i*2;
+            };
+            if (i*2+1 <= this.count && this.arr[maxPos] < this.arr[i*2+1]) {
+                maxPos = i*2+1;
+            };
+            // 当最大值在当前操作节点时打断循环
+            if (maxPos === i) {
+                break;
+            }
+            // 如果没有被打断就将操作节点的值交换到对应子节点，
+            // 操作节点也对应下移。
             [this.arr[i], this.arr[maxPos]] = [this.arr[maxPos], this.arr[i]];
             i = maxPos;
         }
     }
-
     display() {
         return this.arr;
     }
 }
 
-let arr = [12, 2, 22, 42, 32, 21, 24, 25];
-let ex = new Heap().init(15).insertArr(arr);
+let arr = [12, 2, 24, 25, 22, 42, 32, 21];
+let ex = new Heap().init(30).insertArr(arr);
 console.log(ex.display());
 console.log(ex.removeTop());
 console.log(ex.display());
-
